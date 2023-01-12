1c770d1eaf01c156fc4a2eea1457f08f2e756026
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-01-12 11:13:07 -0500

    Update codeChallenge2

--- codeChallenges/codeChallenge2.js
+++ codeChallenges/codeChallenge2.js
@@ -3,10 +3,8 @@ var age = 22;
 
 if (age > 25 && goodDrivingRecord) {
     console.log(`You qualify for a discount on your car rental!`)
-}
-else if (age > 25 || goodDrivingRecord) {
+}else if (age > 25 || goodDrivingRecord) {
     console.log(`You qualify for a car rental.`)
-}
-else {
+}else {
     console.log(`You do not qualify for a car rental please have someone else sign.`)
 }
